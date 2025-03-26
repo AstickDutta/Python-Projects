bf1cf3250a397f9ce4740167470dafe8c7edbec7
rittick dutta <astickdutta19@gmail.com>
2025-03-27 00:14:12 +0530

    question answers

--- practice/prac.py
+++ practice/prac.py
@@ -388,10 +388,25 @@
 #     print("The word 'Python' is not present in the file")
 
 
+# class employee():
+#     salary = 1234000
+#     language = "English"  #class attribute
+
+# astick = employee()
+# astick.name = "harry" #instance atribute
+# print(astick.salary, astick.language, astick.name)
+
 class employee():
     salary = 1234000
-    language = "English"  #class attribute
+    language = "English"  #this is class attribute
 
-astick = employee()
-astick.name = "harry" #instance atribute
-print(astick.salary, astick.language, astick.name)
+    def getInfo(self):
+        print(f"Employee's salary is {self.salary}, language is {self.language}")
+    @staticmethod
+    def greet():
+        print("Hello, I am an employee!")
+
+astick = employee() # this is instance atribute
+
+astick.getInfo()
+astick.greet()
