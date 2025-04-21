fb7809611a0388a00c125d4424a367f55fd6f250
AstickDutta <astickdutta19@gmail.com>
2025-04-21 12:30:38 +0530

    changes in few functions

--- Email-Address-Extractor-Python/emailExtractor.py
+++ Email-Address-Extractor-Python/emailExtractor.py
@@ -1,9 +1,13 @@
-email = input("Please enter your email address: ")
+def email_extractor():
+    email = input("Please enter your email address: ")
 
-if "@" in email:
-    index = email.index("@")
-    userName = email[:index]
-    domain = email[index + 1:]
-    print(f"User Name: {userName} and Domain: {domain}")
-else:
-    print("Invalid email address. Please include '@' in the email.")
+    if "@" in email:
+        index = email.index("@")
+        userName = email[:index]
+        domain = email[index + 1:]
+
+        print(f"User Name : { userName } and Domain: { domain }")
+    else:
+        print("Invalid email address. Please include '@' in the email.")
+
+email_extractor()
