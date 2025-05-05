4e8e1c6a9a459b09b5783964f4f0136f049defe0
AstickDutta <astickdutta19@gmail.com>
2025-05-05 15:12:28 +0530

    changes in practice file

--- practice/mock/remove_kye_thresold.py
+++ practice/mock/remove_kye_thresold.py
@@ -1,10 +1,23 @@
+# def remove_keys_below_threshold(dict1, threshold):
+#     result = {}
+#     for key, value in dict1.items():
+#         if threshold <= value:
+#             result[key] = value
+#     return result
+
+# dict1 = {"a": 12, "j": 4, "u": 23, "p": 89, "l": 30}
+# threshold = int(input("Please enter a number: "))
+# print(remove_keys_below_threshold(dict1, threshold))
+
+
 def remove_keys_below_threshold(dict1, threshold):
     result = {}
-    for key, value in dict1.items():
-        if threshold <= value:
-            result[key] = value
+    for item in dict1:
+        if threshold < dict1[item]:
+            result[item] = dict1[item]
     return result
 
 dict1 = {"a": 12, "j": 4, "u": 23, "p": 89, "l": 30}
 threshold = int(input("Please enter a number: "))
 print(remove_keys_below_threshold(dict1, threshold))
+
