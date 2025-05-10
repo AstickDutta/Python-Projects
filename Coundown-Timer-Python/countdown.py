ae241cbeb95783a847bdc3712bd4f0fc96c0d867
AstickDutta <astickdutta19@gmail.com>
2025-05-10 12:07:39 +0530

    add few problems in prac

--- Coundown-Timer-Python/countdown.py
+++ Coundown-Timer-Python/countdown.py
@@ -1,15 +1,15 @@
 
 import time
-# my_time = int(input("Enter the time in seconds: "))
+my_time = int(input("Enter the time in seconds: "))
 
-# for i in range(my_time, 0, -1):
-#     second = i % 60
-#     minutes = (i // 60) % 60
-#     hours = i // 3600
-#     print(f"{hours:02}:{minutes:02}:{second:02}")
-#     time.sleep(1)
+for i in range(my_time, 0, -1):
+    second = i % 60
+    minutes = (i // 60) % 60
+    hours = i // 3600
+    print(f"{hours:02}:{minutes:02}:{second:02}")
+    time.sleep(1)
 
-# print("Time's up!")
+print("Time's up!")
 
 def countdown_timer():
     my_time = int(input("Enter the time in seconds: "))
